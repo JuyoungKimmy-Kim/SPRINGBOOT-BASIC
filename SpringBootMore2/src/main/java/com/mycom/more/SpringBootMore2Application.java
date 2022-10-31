7b9c139e6b00b6020dc51f68acb052706c19e077
DESKTOP-KVCQHCD\SSAFY <kimmykim.jy@gmail.com>
2022-10-31 18:00:59 +0900

    221031

---
+++ SpringBootMore2/src/main/java/com/mycom/more/SpringBootMore2Application.java
@@ -0,0 +1,13 @@
+package com.mycom.more;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMore2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMore2Application.class, args);
+	}
+
+}
